ffcd94038282c599a9cc2c3f46efc5076d000049
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2021-12-10 17:21:07 +0000

    python

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export BUILD_DIR=$TOP_DIR/build
 export CSHARP_DIR=$TOP_DIR/csharp/tests
 
 function install_packages() {
-    apt install -y meson libfmt-dev python3.8 python-pybind11 
+    apt install -y meson libfmt-dev python python-pybind11 
 }
 
 function cleanup_all() {
