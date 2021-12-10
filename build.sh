3409ec371d7b778476b102de9c3a525a54c07ba3
Alexander Zvyagin <Zvyagin.Alexander@gmail.com>
2021-12-10 16:31:25 +0000

    Add fmt, fix function ()

--- build.sh
+++ build.sh
@@ -2,32 +2,32 @@ export TOP_DIR=$PWD
 export BUILD_DIR=$TOP_DIR/build
 export CSHARP_DIR=$TOP_DIR/csharp/tests
 
-function install_packages {
-    apt install -y meson
+function install_packages() {
+    apt install -y meson fmt
 }
 
-function cleanup_all {
+function cleanup_all() {
     rm -rf $BUILD_DIR
     rm -rf $CSHARP_DIR/bin
 }
 
-function build_cpp {
+function build_cpp() {
     meson $BUILD_DIR
     cd $BUILD_DIR
     ninja
 }
 
-function build_csharp {
+function build_csharp() {
     cd $CSHARP_DIR
     dotnet build
 }
 
-function test_csharp {
+function test_csharp() {
     cd $CSHARP_DIR
     dotnet test
 }
 
-function publish_cpp_to_csharp {
+function publish_cpp_to_csharp() {
     mkdir -p $CSHARP_DIR/bin/Debug/net5.0
     cp -a $BUILD_DIR/csharp/* $CSHARP_DIR/bin/Debug/net5.0
 }
